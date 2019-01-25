922f102f6d7e00c29535b2adf5610b8a2a461ac4
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-25 12:27:48 +0100

    adding naviagtion drawer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <v-app>
-    
+    <v-toolbar>
+      <v-toolbar-side-icon @click.native="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+      
+    </v-navigation-drawer>
+    <main>
+      
+    </main>
   </v-app>
 </template>
 
@@ -14,6 +30,7 @@ export default {
   },
   data () {
     return {
+      sideNav : false
       //
     }
   }

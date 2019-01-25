6b8d9fd90a9d42cac230900088e8700b2aff7770
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-25 14:20:26 +0100

    adding vue-router

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,24 @@
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>
-          View Meetups
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-navigation-drawer v-model="sideNav">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{ item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{ item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <main>
-      
+      <router-view></router-view>
     </main>
   </v-app>
 </template>
@@ -37,8 +37,14 @@ export default {
   },
   data () {
     return {
-      sideNav : false
-      //
+      sideNav : false,
+      menuItems : [
+        { icon : 'supervisor_account', title : 'View Meetups'},
+        { icon : 'room', title : 'Organize Meetup'},
+        { icon : 'person', title : 'profile'},
+        { icon : 'face', title : 'Sign up'},
+        { icon : 'lock_open', title : 'Sign in'}
+      ]
     }
   }
 }

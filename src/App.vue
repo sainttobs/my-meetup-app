820036e2962ec9fa499856f9e393eca876a27e07
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-01-30 07:46:44 +0100

    adding carousel on home page

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,11 @@
     </v-toolbar>
     <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-list-tile temporary v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>{{ item.title}}</v-list-tile-content>
+          <v-list-tile-content temporary>{{ item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,38 @@
 <template>
-	<div>
-		<p>The Home Page </p>
-	</div>
+	<v-container>
+		<v-layout row wrap>
+			<v-flex xs12 sm6 class="text-xs-center text-sm-right">
+				<v-btn class="primary" large router to="/meetups">
+					Explore Meetups
+				</v-btn>
+			</v-flex>
+			<v-flex xs12 sm6 class="text-xs-center text-sm-left">
+				<v-btn class="primary" large router to="/meetups">Explore Meetups</v-btn>
+			</v-flex>
+		</v-layout>
+		<v-layout>
+			<v-flex xs12>
+				<v-carousel>
+				    <v-carousel-item
+				      v-for="meetup in meetups"
+				      :key="meetup.id"
+				      :src="meetup.imageUrl"
+				    ></v-carousel-item>
+				</v-carousel>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
+
+<script>
+	export default {
+		data () {
+			return {
+				meetups: [
+					{ imageUrl: 'https://travel.jumia.com/blog/ng/wp-content/uploads/2015/08/victoria-island-660x400.jpg', id: 'lag_meetup', title: 'Meetup in Lagos'},
+					{ imageUrl: 'https://2.bp.blogspot.com/-JwDu33EhI-Q/Vqk8OnUECPI/AAAAAAAA83Q/pG0vThluJZM/s1600/ibadan%2Bcity%2Bbeautiful.jpg', id: 'ib_meetup', title: 'Meetup in Ibadan'}
+				]
+			}
+		}
+	}
+</script>

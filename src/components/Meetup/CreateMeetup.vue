0371835022dfa62edead43e39f2f63a6f6fc08fc
sainttobs <sainttobs@gmail.com>
2019-03-27 02:14:39 +0100

    adding input to create meetup form

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -2,7 +2,7 @@
 	<v-container>
 		<v-layout row>
 			<v-flex xs12>
-				<h4 class="primary--text">Create a new Meetup </h4>
+				<h2 class="secondary--text">Create a new Meetup </h2>
 			</v-flex>
 		</v-layout>
 		<v-layout>
@@ -14,10 +14,61 @@
 								name="Title"
 								id="title"
 								label="Title"
-								required="true"
+								v-model="title"
+								required
 								clearable
 								solo
 							></v-text-field>
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12 sm6>
+							<v-text-field
+								name="Location"
+								id="location"
+								label="Location"
+								v-model="location"
+								required
+								clearable
+								solo
+							></v-text-field>
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12 sm6>
+							<v-text-field
+								name="imageUrl"
+								id="image-url"
+								label="Image Url"
+								v-model="imageUrl"
+								required
+								clearable
+								solo
+							></v-text-field>
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12 sm6>
+							<img :src="imageUrl">
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12 sm6>
+							<v-text-field
+								name="description"
+								id="description"
+								label="Description"
+								v-model="description"
+								multi-line
+								required
+								clearable
+								solo
+							></v-text-field>
+						</v-flex>
+					</v-layout>
+					<v-layout row>
+						<v-flex xs12 sm6 offset3>
+							<v-btn class="primary"> Create Meetup</v-btn>
 						</v-flex>
 					</v-layout>
 				</form>
@@ -25,3 +76,16 @@
 		</v-layout>
 	</v-container>
 </template>
+
+<script>
+export default {
+	data () {
+		return {
+			title : '',
+			location : '',
+			imageUrl: '',
+			description: ''
+		}
+	}
+}
+</script>

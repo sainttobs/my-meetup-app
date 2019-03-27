9633efa050bd75688b84d889e9e506a0f43b2b51
sainttobs <sainttobs@gmail.com>
2019-03-27 01:51:18 +0100

    adding create meetups page

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,27 @@
 <template>
-	<div>
-		<p> The Create Meetups Page</p>
-	</div>
+	<v-container>
+		<v-layout row>
+			<v-flex xs12>
+				<h4 class="primary--text">Create a new Meetup </h4>
+			</v-flex>
+		</v-layout>
+		<v-layout>
+			<v-flex xs12>
+				<form>
+					<v-layout row>
+						<v-flex xs12 sm6>
+							<v-text-field
+								name="Title"
+								id="title"
+								label="Title"
+								required="true"
+								clearable
+								solo
+							></v-text-field>
+						</v-flex>
+					</v-layout>
+				</form>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>

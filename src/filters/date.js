1c1fc73e730649629daa493b63bc580b23764416
sainttobs <sainttobs@gmail.com>
2019-04-23 04:17:18 +0100

    adding date filter

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,1 +1,4 @@
-date.js
+export default (value) => {
+	const date = new Date(value)
+	return date.toLocaleString(['en-US'], {month: 'short', day: '2-digit', year:'numeric', hour: '2-digit', minute: '2-digit'})
+}
